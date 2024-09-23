24f09bf7e81bf8d355e12ed26f41c8c155c057e9
evildevill <hackerbad669@gmail.com>
2024-09-23 19:05:45 +0500

    initial commit

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+        <h1 className="text-2xl font-bold text-center text-gray-900">About</h1>
+        <p className="text-gray-600 text-center">
+          This is the About page, here you can find more infor about us.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
