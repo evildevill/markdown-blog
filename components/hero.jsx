3717d9d4c45fc23ad4a9e5eb676365473f4826bd
evildevill <hackerbad669@gmail.com>
2024-09-24 15:22:15 +0500

    text changes

--- components/hero.jsx
+++ components/hero.jsx
@@ -6,12 +6,12 @@ export function Hero() {
     <section className="container px-4 py-10 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center">
       <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
         <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
-          A <span className="font-semibold">free repository</span> for community <br className="hidden lg:block" /> 
-          components using <span className="font-semibold underline decoration-primary">Tailwind CSS</span>
+          A personal blog offering <span className="font-semibold">free resources, insights,</span> and tools on<br className="hidden lg:block" />
+          <span className="font-semibold underline decoration-primary">cybersecurity and programming,</span>
         </h1>
         <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-          Open source Tailwind UI components and templates to <br className="hidden lg:block" /> 
-          bootstrap your new apps, projects or landing sites!
+          designed to help others enhance their skills <br className="hidden lg:block" />
+          and stay updated with the latest industry trends.
         </p>
         <div className="mt-6 bg-transparent border rounded-lg dark:border-gray-700 lg:w-2/3 focus-within:border-primary focus-within:ring focus-within:ring-primary dark:focus-within:border-primary focus-within:ring-opacity-20">
           <form className="flex flex-wrap justify-between md:flex-row">
