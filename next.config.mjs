be6b3a869903dfd51eedcd9570292f5ea2007913
evildevill <hackerbad669@gmail.com>
2024-10-05 17:14:50 +0500

    added redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/product',
+            destination: '/blogposts/how-hackers-target-instagram-accounts-protect-yourself-2024',
+            permanent: true, // This indicates a 301 permanent redirect
+          },
+        ];
+      },
+};
 
 export default nextConfig;
