8841a6f302e38efdc994ccf9ed82568d1fb9330a
evildevill <hackerbad669@gmail.com>
2024-10-13 20:54:24 +0500

    updated terms

--- app/legal/terms/page.js
+++ app/legal/terms/page.js
@@ -1,9 +1,166 @@
-import React from 'react'
+import React from "react";
 
 const Terms = () => {
   return (
-    <div>Terms</div>
-  )
-}
+    <div className="p-8 min-h-screen">
+      <div className="max-w-3xl mx-auto p-6 rounded">
+        <h1 className="text-3xl font-bold mb-6">
+          Terms of Service
+        </h1>
+        <p className="font-bold pb-8">Last updated October 13, 2024</p>
 
-export default Terms
+        <section className="mb-4">
+          <h2 className="text-2xl font-semibold mb-2">AGREEMENT TO OUR LEGAL TERMS</h2>
+          <p className="text-base">
+            We are Waseem Akram (&quot;Company,&quot; &quot;we,&quot; &quot;us,&quot; &quot;our&quot;), a company registered in Pakistan.
+          </p>
+        </section>
+        <section className="mb-4">
+          <p className="text-base">
+            We operate the website https://hackerwasii.com (the &quot;Site&quot;), as well as any other related products and services that refer or link to these legal terms (the &quot;Legal Terms&quot;) (collectively, the &quot;Services&quot;).
+          </p>
+        </section>
+        <section className="mb-4">
+          <p className="text-base">
+            You can contact us by email at hello@hackerwasii.com
+          </p>
+        </section>
+        <section className="mb-4">
+          <p className="text-base">
+            These Legal Terms constitute a legally binding agreement made between you, whether personally or on behalf of an entity (&quot;you&quot;), and Waseem Akram, concerning your access to and use of the Services. You agree that by accessing the Services, you have read, understood, and agreed to be bound by all of these Legal Terms. IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.
+          </p>
+        </section>
+        <section className="mb-4">
+          <p className="text-base">
+            We will provide you with prior notice of any scheduled changes to the Services you are using. The modified Legal Terms will become effective upon posting or notifying you by hello@hackerwasii.com, as stated in the email message. By continuing to use the Services after the effective date of any changes, you agree to be bound by the modified terms.
+            <br />
+            <br />
+            All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Services. If you are a minor, you must have your parent or guardian read and agree to these Legal Terms prior to you using the Services.
+            <br />
+            <br />
+            We recommend that you print a copy of these Legal Terms for your records.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="text-2xl font-semibold mb-2">1. OUR SERVICES</h2>
+          <p className="text-base">
+            The information provided when using the Services is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Services from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.
+            <br />
+            <br />
+            The Services are not tailored to comply with industry-specific regulations (Health Insurance Portability and Accountability Act (HIPAA), Federal Information Security Management Act (FISMA), etc.), so if your interactions would be subjected to such laws, you may not use the Services. You may not use the Services in a way that would violate the Gramm-Leach-Bliley Act (GLBA).
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="text-2xl font-semibold mb-2">2. INTELLECTUAL PROPERTY RIGHTS</h2>
+          <p className="text-base">
+            Our intellectual property
+            <br />
+            <br />
+            We are the owner or the licensee of all intellectual property rights in our Services, including all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics in the Services (collectively, the &quot;Content&quot;), as well as the trademarks, service marks, and logos contained therein (the &quot;Marks&quot;).
+            <br />
+            <br />
+            Our Content and Marks are protected by copyright and trademark laws (and various other intellectual property rights and unfair competition laws) and treaties in the United States and around the world.
+            <br />
+            <br />
+            The Content and Marks are provided in or through the Services &quot;AS IS&quot; for your personal, non-commercial use only.
+            <br />
+            <br />
+            Your use of our Services
+            <br />
+            <br />
+            Subject to your compliance with these Legal Terms, including the &quot;PROHIBITED ACTIVITIES&quot; section below, we grant you a non-exclusive, non-transferable, revocable license to:
+            access the Services; and
+            download or print a copy of any portion of the Content to which you have properly gained access.
+            solely for your personal, non-commercial use.
+            <br />
+            <br />
+            Except as set out in this section or elsewhere in our Legal Terms, no part of the Services and no Content or Marks may be copied, reproduced, aggregated, republished, uploaded, posted, publicly displayed, encoded, translated, transmitted, distributed, sold, licensed, or otherwise exploited for any commercial purpose whatsoever, without our express prior written permission.
+            <br />
+            <br />
+            If you wish to make any use of the Services, Content, or Marks other than as set out in this section or elsewhere in our Legal Terms, please address your request to: hello@hackerwasii.com. If we ever grant you the permission to post, reproduce, or publicly display any part of our Services or Content, you must identify us as the owners or licensors of the Services, Content, or Marks and ensure that any copyright or proprietary notice appears or is visible on posting, reproducing, or displaying our Content.
+            <br />
+            <br />
+            We reserve all rights not expressly granted to you in and to the Services, Content, and Marks.
+            <br />
+            <br />
+            Any breach of these Intellectual Property Rights will constitute a material breach of our Legal Terms and your right to use our Services will terminate immediately.
+            <br />
+            <br />
+            Your submissions
+            <br />
+            <br />
+            Please review this section and the &quot;PROHIBITED ACTIVITIES&quot; section carefully prior to using our Services to understand the (a) rights you give us and (b) obligations you have when you post or upload any content through the Services.
+            <br />
+            <br />
+            Submissions: By directly sending us any question, comment, suggestion, idea, feedback, or other information about the Services (&quot;Submissions&quot;), you agree to assign to us all intellectual property rights in such Submission. You agree that we shall own this Submission and be entitled to its unrestricted use and dissemination for any lawful purpose, commercial or otherwise, without acknowledgment or compensation to you.
+            <br />
+            <br />
+            You are responsible for what you post or upload: By sending us Submissions through any part of the Services you:
+            confirm that you have read and agree with our &quot;PROHIBITED ACTIVITIES&quot; and will not post, send, publish, upload, or transmit through the Services any Submission that is illegal, harassing, hateful, harmful, defamatory, obscene, bullying, abusive, discriminatory, threatening to any person or group, sexually explicit, false, inaccurate, deceitful, or misleading;
+            to the extent permissible by applicable law, waive any and all moral rights to any such Submission;
+            warrant that any such Submission are original to you or that you have the necessary rights and licenses to submit such Submissions and that you have full authority to grant us the above-mentioned rights in relation to your Submissions; and
+            warrant and represent that your Submissions do not constitute confidential information.
+            You are solely responsible for your Submissions and you expressly agree to reimburse us for any and all losses that we may suffer because of your breach of (a) this section, (b) any third party’s intellectual property rights, or (c) applicable law.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="text-2xl font-semibold mb-2">3. USER REPRESENTATIONS</h2>
+          <p className="text-base">
+            By using the Services, you represent and warrant that: (1) all registration information you submit will be true, accurate, current, and complete; (2) you will maintain the accuracy of such information and promptly update such registration information as necessary; (3) you have the legal capacity and you agree to comply with these Legal Terms; (4) you are not a minor in the jurisdiction in which you reside, or if a minor, you have received parental permission to use the Services; (5) you will not access the Services through automated or non-human means, whether through a bot, script or otherwise; (6) you will not use the Services for any illegal or unauthorized purpose; and (7) your use of the Services will not violate any applicable law or regulation.
+            <br />
+            <br />
+            If you provide any information that is untrue, inaccurate, not current, or incomplete, we have the right to suspend or terminate your account and refuse any and all current or future use of the Services (or any portion thereof).
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="text-2xl font-semibold mb-2">4. USER REGISTRATION</h2>
+          <p className="text-base">
+            You may be required to register to use the Services. You agree to keep your password confidential and will be responsible for all use of your account and password. We reserve the right to remove, reclaim, or change a username you select if we determine, in our sole discretion, that such username is inappropriate, obscene, or otherwise objectionable.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="text-2xl font-semibold mb-2">5. PURCHASES AND PAYMENT</h2>
+          <p className="text-base">
+            We accept the following forms of payment:
+          </p>
+          <ul className="list-disc pl-5">
+            <li>Visa</li>
+            <li>Mastercard</li>
+            <li>American Express</li>
+            <li>PayPal</li>
+            <li>Discover</li>
+          </ul>
+          <br />
+          <p className="text-base">
+            You agree to provide current, complete, and accurate purchase and account information for all purchases made via the Services. You further agree to promptly update account and payment information, including email address, payment method, and payment card expiration date, so that we can complete your transactions and contact you as needed. Sales tax will be added to the price of purchases as deemed required by us. We may change prices at any time. All payments shall be in US dollars.
+            <br />
+            <br />
+            You agree to pay all charges at the prices then in effect for your purchases and any applicable shipping fees, and you authorize us to charge your chosen payment provider for any such amounts upon placing your order. We reserve the right to correct any errors or mistakes in pricing, even if we have already requested or received payment.
+            <br />
+            <br />
+            We reserve the right to refuse any order placed through the Services. We may, in our sole discretion, limit or cancel quantities purchased per person, per household, or per order. These restrictions may include orders placed by or under the same customer account, the same payment method, and/or orders that use the same billing or shipping address. We reserve the right to limit or prohibit orders that, in our sole judgment, appear to be placed by dealers, resellers, or distributors.
+          </p>
+        </section>
+        <section className="mb-4">
+          <h2 className="text-2xl font-semibold mb-2">6. SUBSCRIPTIONS</h2>
+          <h3 className="text-sm font-bold mb-3">Billing and Renewal</h3>
+          <p className="text-base">
+          Your subscription will continue and automatically renew unless canceled. You consent to our charging your payment method on a recurring basis without requiring your prior approval for each recurring charge, until such time as you cancel the applicable order. The length of your billing cycle is monthly.
+          </p>
+          <br />
+          <h3 className="text-sm font-bold mb-3">Cancellation</h3>
+          <p className="text-base">
+          All purchases are non-refundable. You can cancel your subscription at any time by logging into your account. Your cancellation will take effect at the end of the current paid term. If you have any questions or are unsatisfied with our Services, please email us at hello@hackerwasii.com.
+          </p>
+          <br />
+          <h3 className="text-sm font-bold mb-3">Fee Changes</h3>
+          <p className="text-base">
+          We may, from time to time, make changes to the subscription fee and will communicate any price changes to you in accordance with applicable law.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Terms;
