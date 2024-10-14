24f7b506ac452a073841d4e7a383e9e3cdf29957
evildevill <hackerbad669@gmail.com>
2024-10-14 12:34:34 +0500

    added privacy, updated footerbar

--- app/legal/privacy-policy/page.js
+++ app/legal/privacy-policy/page.js
@@ -1,9 +1,230 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const PrivacyPolicy = () => {
   return (
-    <div>Privacy Policy</div>
-  )
-}
+    <div className="p-8 min-h-screen">
+      <div className="max-w-3xl mx-auto p-6 rounded">
+        <h1 className="text-3xl font-bold mb-6">Privacy Policy</h1>
+        <p className="font-bold pb-8">Last updated October 13, 2024</p>
+        <p className="text-base">
+          At Hackerwasii, accessible from{" "}
+          <Link href="https://hackerwasii.com" className="text-blue-500 hover:underline">https://hackerwasii.com
+          </Link>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Hackerwasii and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. <br /><br /> This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Hackerwasii. This policy is not applicable to any information collected offline or via channels other than this website.
+        </p>
+        <br /><br />
+        <h2 className="text-2xl font-semibold mb-2">Information we collect</h2>
+        <p className="text-base">
+          <br />
+          The personal information that you are asked to provide, and the
+          reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information. <br /><br />If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide. <br /><br />When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number. <br /><br />
+        </p>
 
-export default PrivacyPolicy
+        <h2 className="text-2xl font-semibold mb-2">
+          How we use your information
+        </h2>
+        <p className="text-base">We use the information we collect in various ways, including to:</p>
+        <ul className="list-disc list-inside">
+          <li>Provide, operate, and maintain our website</li>
+          <li>Improve, personalize, and expand our website</li>
+          <li>Understand and analyze how you use our website</li>
+          <li>Develop new products, services, features, and functionality</li>
+          <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with and other information relating to the website, and for marketing and promotional purposes
+          </li>
+          <li>Send you emails</li>
+          <li>Find and prevent fraud</li>
+          <br />
+        </ul>
+
+        <h2 className="text-2xl font-semibold mb-2">Log Files</h2>
+        <p className="text-base">
+          Hackerwasii follows a standard procedure of using log files. These
+          files log visitors when they visit websites. All hosting companies do
+          this and a part of hosting services analytics. The information
+          collected by log files include internet protocol (IP) addresses,
+          browser type, Internet Service Provider (ISP), date and time stamp,
+          referring/exit pages, and possibly the number of clicks. These are not
+          linked to any information that is personally identifiable. The purpose
+          of the information is for analyzing trends, administering the site,
+          tracking users movement on the website, and gathering demographic
+          information. <br /><br />
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">Cookies and Web Beacons</h2>
+        <p className="text-base">Like any other website, Hackerwasii Design uses &quot;cookies&quot;. These cookies are used to store information including visitors&quot; preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users experience by customizing our web page content based on visitors browser type and/or other information. <br /><br /> For more general information on cookies, please read{" "}
+          <Link
+            href="https://policies.google.com/technologies/cookies"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          > What are Cookies?</Link>.<br /><br />
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">Google DoubleClick DART Cookie</h2>
+        <p className="text-base">
+          Google is one of a third-party vendor on our site. It also uses
+          cookies, known as DART cookies, to serve ads to our site visitors
+          based upon their visit to www.website.com and other sites on the
+          internet. However, visitors may choose to decline the use of DART
+          cookies by visiting the Google ad and content network Privacy Policy
+          at the following URL –
+          <Link
+            href="https://policies.google.com/technologies/ads"
+            className="text-blue-500 hover:underline"
+          >
+            https://policies.google.com/technologies/ads
+          </Link><br /><br />
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">
+          Our Advertising Partners
+        </h2>
+        <p className="text-base">
+          Some of advertisers on our site may use cookies and web beacons. Our
+          advertising partners are listed below. Each of our advertising
+          partners has their own Privacy Policy for their policies on user data.
+          For easier access, we hyperlinked to their Privacy Policies below.
+        </p>
+        <ul className="list-disc list-inside">
+          <li>
+            Google -{" "}
+            <Link
+              href="https://policies.google.com/technologies/ads"
+              className="text-blue-500 hover:underline"
+            >
+              Privacy Policy
+            </Link>
+          </li>
+        </ul>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">
+          Advertising Partners Privacy Policies
+        </h2>
+
+        <p className="text-base">
+          You may consult this list to find the Privacy Policy for each of the advertising partners of Hackerwasii. <br /><br />
+        </p>
+        <p className="text-base">
+          Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Hackerwasii, which are sent directly to users browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit. <br /><br />
+        </p>
+        <p className="text-base">
+          Note that Hackerwasii has no access to or control over these cookies that are used by third-party advertisers.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">Third Party Privacy Policies</h2>
+
+        <p className="text-base">
+          Hackerwasii&quot;s Privacy Policy does not apply to other advertisers
+          or websites. Thus, we are advising you to consult the respective
+          Privacy Policies of these third-party ad servers for more detailed
+          information. It may include their practices and instructions about how to opt-out of certain options. <br /><br />
+        </p>
+        <p className="text-base">
+          You can choose to disable cookies through your individual browser
+          options. To know more detailed information about cookie management
+          with specific web browsers, it can be found at the browsers&quot;
+          respective websites.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">Links To External Sites</h2>
+        <br />
+        <p className="text-base">
+          Our Service may contain links to external sites that are not operated by us.
+        </p>
+        <p className="text-base">
+          If you click on a third party link, you will be directed to that third party’s site. We strongly advise you to review the Privacy Policy and the terms and conditions of every site you visit.
+          We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites, products or services.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">
+          CCPA Privacy Rights (Do Not Sell My Personal Information)
+        </h2>
+
+        <p className="text-base">
+          Under the CCPA, among other rights, California consumers have the
+          right to:
+        </p>
+        <p className="text-base">
+          Request that a business that collects a consumer&quot;s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+        </p>
+        <p className="text-base">
+          Request that a business delete any personal data about the consumer
+          that a business has collected. Request that a business that sells a consumer&quot;s personal data, not sell the consumer&quot;s personal data. If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">GDPR Data Protection Rights</h2>
+
+        <p className="text-base">
+          We would like to make sure you are fully aware of all of your data
+          protection rights. Every user is entitled to the following:
+
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service.
+
+          The right to rectification – You have the right to request that we
+          correct any information you believe is inaccurate. You also have the
+          right to request that we complete the information you believe is
+          incomplete.
+
+          The right to erasure – You have the right to request that we erase
+          your personal data, under certain conditions.
+
+          The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
+
+          The right to object to processing – You have the right to object to
+          our processing of your personal data, under certain conditions.
+
+          The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">Children&quot;s Information</h2>
+        <p className="text-base">
+          Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity. Hackerwasii does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">Privacy Policy Changes</h2>
+
+        <p className="text-base">
+          Although most changes are likely to be minor, Hackerwasii may change its Privacy Policy from time to time, and in Hackerwasii&quot;s sole discretion. When we do, we will revise the updated date at the top of this page. Hackerwasii encourages visitors to frequently check this page for any changes to its Privacy Policy.
+          Your continued use of this site after any change in this Privacy
+          Policy will constitute your acceptance of such change.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">Acceptance of this Policy</h2>
+
+        <p className="text-base">
+          You acknowledge that you have read this Policy and agree to all its
+          terms and conditions. By accessing and using the Website and Services
+          you agree to be bound by this Policy. If you do not agree to abide by
+          the terms of this Policy, you are not authorized to access or use the
+          Website and Services.
+        </p>
+        <br />
+        <h2 className="text-2xl font-semibold mb-2">Contacting us</h2>
+
+        <p className="text-base">
+          If you have any queries related to the privacy policy or usage or
+          capture of information, feel free to contact us:
+        </p>
+
+        <ul className="list-disc list-inside">
+          <li>
+            By sending an Email to: <br />
+            <Link
+              href="mailto:support@hackerwasii.com?subject=Queries%20related%20to%20the%20Privacy%20Policy%20of%20%27Hackerwasii%20Blog%27%20Website"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            > support@hackerwasii.com
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
