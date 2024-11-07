2a3b868c170242aff06acf2069156ead913f9cb0
evildevill <hackerbad669@gmail.com>
2024-11-07 11:57:05 +0500

    added meta tags for signup page

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,12 +9,36 @@ import {
 } from "@/components/ui/card"
 import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
-export const description =
-  "A sign up form with an option to sign up with GitHub and a link to login if you already have an account";
+export const metadata = {
+  title: "Sign Up - Waseem Akram",
+  description: "Join the community! Sign up now to get access to exclusive content, updates, and insights on coding, cybersecurity, and tech projects.",
+  openGraph: {
+    title: "Sign Up - Waseem Akram",
+    description: "Join the community! Sign up now to get access to exclusive content, updates, and insights on coding, cybersecurity, and tech projects.",
+    url: "https://hackerwasii.com/signup",
+    siteName: "Waseem Akram – Researcher, Pentester, Dev",
+    // images: [
+    //   {
+    //     url: "https://hackerwasii.com/images/signup-banner.jpg",
+    //     width: 1200,
+    //     height: 630,
+    //     alt: "Sign Up with Waseem Akram",
+    //   }
+    // ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sign Up - Waseem Akram",
+    description: "Join the community! Sign up now to get access to exclusive content, updates, and insights on coding, cybersecurity, and tech projects.",
+    image: "https://hackerwasii.com/images/signup-banner.jpg",
+    creator: "@wasii_254",
+  },
+};
 
 const Signup = () => {
   return (
-    <div className="pt-56 pb-48">
+    <div className="pt-56 pb-56">
       <Card className="mx-auto max-w-sm">
         <CardHeader>
           <CardTitle className="text-xl text-center">Sign Up</CardTitle>
