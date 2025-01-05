3c1586e54aae756dd99c22c332031bb0fc0e4069
evildevill <hackerbad669@gmail.com>
2025-01-05 16:45:29 +0500

    feat: add metadata for Firewall Bypass VU page to improve SEO and sharing

--- app/firewall-bypass-vu/page.js
+++ app/firewall-bypass-vu/page.js
@@ -1,6 +1,39 @@
 import { FirewallVu } from "@/components/firewall-vu";
 import React from "react";
 
+export const metadata = {
+  title: "Firewall Bypass VU - Chrome Extension | Waseem Akram",
+  description:
+    "Enhance your Virtual University experience with Firewall Bypass VU. Skip lecture restrictions, copy quizzes, and easily manage GDB tasks with this powerful Chrome extension.",
+  openGraph: {
+    title: "Firewall Bypass VU - Chrome Extension | Waseem Akram",
+    description:
+      "Unlock seamless access to Virtual University features. Firewall Bypass VU lets you skip lectures, copy quizzes, and manage GDB tasks effortlessly. Your ultimate tool for productivity.",
+    url: "https://hackerwasii.com/firewall-bypass-vu",
+    siteName: "Firewall Bypass VU – Chrome Extension",
+    // Uncomment the below if you have an image
+    // images: [
+    //   {
+    //     url: "https://hackerwasii.com/images/extension-banner.jpg",
+    //     width: 1200,
+    //     height: 630,
+    //     alt: "Firewall Bypass VU - Chrome Extension"
+    //   }
+    // ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Firewall Bypass VU - Chrome Extension | Waseem Akram",
+    description:
+      "Streamline your Virtual University tasks with Firewall Bypass VU. Skip lectures, copy quizzes, and manage GDB tasks with ease using this Chrome extension.",
+    // Uncomment the below if you have an image
+    // image: "https://hackerwasii.com/images/extension-banner.jpg",
+    creator: "@wasii_254",
+  },
+};
+
+
 const FirewallBypass = () => {
   return <FirewallVu />;
 };
