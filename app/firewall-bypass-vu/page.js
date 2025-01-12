5064797f67f4cb9e1ee861aef7b49497505df93e
evildevill <hackerbad669@gmail.com>
2025-01-12 16:47:54 +0500

    feat: add additional meta tags and FAQ section to Firewall Bypass VU page for improved user information

--- app/firewall-bypass-vu/page.js
+++ app/firewall-bypass-vu/page.js
@@ -29,6 +29,63 @@ export const metadata = {
     image: "https://hackerwasii.com/images/firewall-bypass-vu.webp",
     creator: "@wasii_254",
   },
+  additionalMetaTags: [
+    {
+      name: "author",
+      content: "Waseem Akram",
+    },
+    {
+      name: "keywords",
+      content:
+        "Firewall Bypass VU, Chrome Extension, Virtual University, VU, VULMS, VU LMS, Virtual University of Pakistan, VU Chrome Extension, VU LMS Chrome Extension, VU LMS Bypass, VU LMS Quiz Bypass, VU LMS GDB Bypass, VU LMS Lecture Bypass, VU LMS Lecture Skip, VU LMS Quiz Copy, VU LMS GDB Copy, VU LMS GDB Manager, VU LMS GDB Task Manager, VU LMS Quiz Manager, VU LMS Quiz Task Manager, VU LMS Lecture Manager, VU LMS Lecture Task Manager",
+    },
+  ],
+  faq: {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    mainEntity: [
+      {
+        "@type": "Question",
+        name: "What is Firewall Bypass VU?",
+        acceptedAnswer: {
+          "@type": "Answer",
+          text: "Firewall Bypass VU is a Chrome extension that enhances your Virtual University experience. It allows you to skip lecture restrictions, copy quizzes, and easily manage GDB tasks.",
+        },
+      },
+      {
+        "@type": "Question",
+        name: "How to install Firewall Bypass VU?",
+        acceptedAnswer: {
+          "@type": "Answer",
+          text: "You can install Firewall Bypass VU from the Chrome Web Store. Simply search for 'Firewall Bypass VU' and click on the 'Add to Chrome' button to install the extension.",
+        },
+      },
+      {
+        "@type": "Question",
+        name: "Is Firewall Bypass VU safe to use?",
+        acceptedAnswer: {
+          "@type": "Answer",
+          text: "Yes, Firewall Bypass VU is safe to use. The extension does not collect any personal information and is designed to enhance your productivity on the Virtual University platform.",
+        },
+      },
+      {
+        "@type": "Question",
+        name: "Can I use Firewall Bypass VU on other websites?",
+        acceptedAnswer: {
+          "@type": "Answer",
+          text: "No, Firewall Bypass VU is specifically designed for the Virtual University platform. It will not work on other websites.",
+        },
+      },
+      {
+        "@type": "Question",
+        name: "How can I report an issue with Firewall Bypass VU?",
+        acceptedAnswer: {
+          "@type": "Answer",
+          text: "If you encounter any issues with Firewall Bypass VU, you can report them by sending an email to hello@hackewasii.com.",
+        },
+      },
+    ],
+  },
 };
 
 const FirewallBypass = () => {
